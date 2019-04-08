4cb3d7e3ba70fbfd8b72633d2cc09fb0d5a345c5
Otis Mahlare <omahlare@tracker.co.za>
2019-04-08 04:11:34 +0200

    First partial commit

--- answers/answers.md
+++ answers/answers.md
@@ -13,13 +13,13 @@
 
 ### 2. Responsive (15m)
 
-1) add here
+1) Added bootstrap row within section with ID "quote"
 
-2) add here
+2) Added bootstrap row to each and every item in the main section
 
-3) add here
+3) Added bootstrap grid columns for each item input within each row
 
-4) add here
+4) Removed the <p> tage which enclosed all items in the section
 
 
 ### 3. Validation (15m)
@@ -38,6 +38,10 @@ Make sure that the WebHost calls the ServiceHost via REST.
 
 ### 7. ADO.Net (40m)
 Add any SQL schema changes here
+
+Column [LeadId] on [Lead] table as been updated to Identity(1,1).
+Column [LeadParameterId] on [LeadParameter] table as been updated to Identity(1,1).
+All changes are in the attached script named schema_updated_question7.
 
 ### 8. Poll DB (15m)
 Add any SQL schema changes here
@@ -65,7 +69,14 @@ Add any SQL schema changes here
 **Answer**
 
 **SQL**
-`Select....`
+SELECT 
+	profits.CampaignId,
+	profits.[Profit (inc VAT)]
+FROM
+	(SELECT CampaignId,SUM(Earnings)-(SUM(Cost)*1.15) 'Profit (inc VAT)'
+		FROM [LeadDetail]
+		GROUP BY CampaignId ) profits
+WHERE profits.[Profit (inc VAT)] > 0
 
 4) Average conversion rate
 **Answer**
---
+++ schema_updated_question7.sql
@@ -0,0 +1,49 @@
+﻿USE [JAG2019]
+
+GO
+/****** Object:  Table [dbo].[Lead]    Script Date: 2018/11/12 4:48:10 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Lead](
+	[LeadId] [bigint]  IDENTITY(1,1 )NOT NULL,
+	[TrackingCode] [varchar](20) NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[ContactNumber] [varchar](20) NOT NULL,
+	[Email] [varchar](150) NULL,
+	[ReceivedDateTime] [datetime] NOT NULL,
+	[IPAddress] [varchar](50) NULL,
+	[UserAgent] [varchar](500) NULL,
+	[ReferrerURL] [varchar](500) NULL,
+	[IsDuplicate] [bit] NULL,
+	[IsCapped] [bit] NULL,
+	[IsSuccessful] [bit] NULL,
+ CONSTRAINT [PK_Lead] PRIMARY KEY CLUSTERED 
+(
+	[LeadId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[LeadParameter]    Script Date: 2018/11/12 4:48:11 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LeadParameter](
+	[LeadParameterId] [bigint] IDENTITY(1,1) NOT NULL,
+	[LeadId] [bigint] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Value] [nvarchar](200) NOT NULL,
+ CONSTRAINT [PK_LeadParameter] PRIMARY KEY CLUSTERED 
+(
+	[LeadParameterId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[LeadParameter]  WITH CHECK ADD  CONSTRAINT [FK_LeadParameter_Lead] FOREIGN KEY([LeadId])
+REFERENCES [dbo].[Lead] ([LeadId])
+GO
+ALTER TABLE [dbo].[LeadParameter] CHECK CONSTRAINT [FK_LeadParameter_Lead]
+GO
